e7f94d22e780e8def4b6abce3416f40685afeac5
Dominic Farquharson <farquharsondominic@gmail.com>
2017-03-02 20:05:36 -0500

    Adding React Boostrap

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,40 @@
 import React, {Component} from 'react';
 import  {Link}  from 'react-router';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap'
+
+
+const navbarInstance = (
+  <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">Game Logger</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} >    <Link to="/"
+            activeOnlyWhenExact
+            activeClassName="underline">
+            Home
+          </Link></NavItem>
+        <NavItem eventKey={2}>
+            <Link to="/AddGame"
+              activeOnlyWhenExact
+              activeClassName="underline">
+              Add Game
+            </Link>
+          </NavItem>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">About</NavItem>
+        <NavItem eventKey={2} href="#">Contact</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+
 
 // Header Component - nav bar
 class Header extends Component {
@@ -12,23 +47,31 @@ class Header extends Component {
   render() {
       const header = this.props;
     return(
-      <div id="header">
-        <h1>{header.title}</h1>
-      <Link to="/"
-      activeOnlyWhenExact
-      activeClassName="underline">
-      Home
-    </Link>
 
-      <Link to="/AddGame"
-      activeOnlyWhenExact
-      activeClassName="underline">
-      Add Game
-      </Link>
-      <hr />
-      </div>
+    <div>
+      
+      {navbarInstance}
+    </div>
+
     )
   }
 }
+
+{/*
+<nav class="img-responsive">
+  <h1>{header.title}</h1>
+<Link to="/"
+activeOnlyWhenExact
+activeClassName="underline">
+Home
+</Link>  */}
+
+{/* <Link to="/AddGame"
+activeOnlyWhenExact
+activeClassName="underline">
+Add Game
+</Link>
+<hr />
+</nav> */}
 
 export default Header;
